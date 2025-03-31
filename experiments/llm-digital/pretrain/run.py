c2889e3fb0d9fdc1077eef28a4ebc5a720df8a3d
Cheng Zhang <chengzhang98@outlook.com>
2025-03-31 23:49:07 +0100

    add mase-triton to deps; add pythonpath in run.py

--- experiments/llm-digital/pretrain/run.py
+++ experiments/llm-digital/pretrain/run.py
@@ -1,3 +1,7 @@
+import sys
+from pathlib import Path
+
+sys.path.append(Path(__file__).resolve().parents[3].joinpath("src").as_posix())
 from typing import Literal, Optional
 import datetime
 import yaml
